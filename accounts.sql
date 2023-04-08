aebe9e9644ab34318d6611ab88006160fe936d64
Shaleen Mathur <shaleenmathur26@csu.fullerton.edu>
2023-04-07 21:47:44 -0700

    made some changes

--- accounts.sql
+++ accounts.sql
@@ -1,14 +1,13 @@
-use midterm;
-CREATE TABLE accounts (
-  `id` INT AUTO_INCREMENT NOT NULL,
-  `username` VARCHAR(50) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `first_name` VARCHAR(255) NOT NULL,
-  `last_name` VARCHAR(255) NOT NULL,
-  `registration_date` DATETIME NOT NULL,
-  `last_login_date` DATETIME DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY (username),
-  UNIQUE KEY (email)
-)ENGINE=InnODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;	
+use 449_db;
+CREATE TABLE IF NOT EXISTS `accounts` (
+`id` int NOT NULL AUTO_INCREMENT,
+`username` varchar(50) NOT NULL,
+`password` varchar(255) NOT NULL,
+`email` varchar(100) NOT NULL,
+`organisation` varchar(100) NOT NULL,
+`address` varchar (100) NOT NULL,
+`city` varchar (100) NOT NULL,
+`state` varchar (100) NOT NULL,
+`country` varchar (100) NOT NULL,
+`postalcode` varchar(100) NOT NULL, PRIMARY KEY (`id`)
+) ENGINE=InnODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
--- app.py
+++ app.py
@@ -1,31 +1,42 @@
-import os
-from flask import Flask, render_template, request, redirect, url_for, session, abort
+from flask import Flask, render_template, request, redirect, url_for, session, abort, jsonify
 import pymysql
-from flask_mysqldb import MySQL
 from flask_cors import CORS
 import re
+import jwt
+import os
 from datetime import timedelta
 from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 
 # CORS(app)
-
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
-app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
-app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
-app.config['UPLOAD_PATH'] = 'uploads'
-app.config['JWT_SECRET_KEY'] = 'super-secret'
 
+app.config["DEBUG"] = True
+app.secret_key = 'happykey'
+app.permanent_session_lifetime = timedelta(minutes = 10) #added the 10 minute session expiry time 
+
+# app.config['MYSQL_HOST'] = '127.0.0.1'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = '1234'
+# app.config['MYSQL_DB'] = 'test'
+# To connect MySQL database
 conn = pymysql.connect(
         host='localhost',
         user='root', 
         password = "shaleen23",
-        db='midterm',
+        db='449_db',
 		cursorclass=pymysql.cursors.DictCursor
         )
 cur = conn.cursor()
-@app.errorhandler(415)
+
+
+@app.errorhandler(404)
 def page_not_found(e):
-	return render_template('415.html'), 415
+	return render_template('404.html'), 404
 
 @app.errorhandler(401)
 def unauthorized(e):
@@ -154,20 +165,71 @@ def admin():
 		abort(401)
 
 
+@app.route('/upload')
+def upload_file():
+   # Make sure they are logged in before accessing the upload page
+   if 'loggedin' in session:
+      return render_template("upload.html")
+   # If not logged in, direct them to the login page
+   return redirect(url_for('login'))
+	
 
-@app.route('/', methods=['POST'])
-def upload_files():
-	uploaded_file = request.files['file']
-	filename = secure_filename(uploaded_file.filename)
-	if filename != '':
-		file_ext = os.path.splitext(filename)[1]
-	if file_ext not in app.config['UPLOAD_EXTENSIONS']:
-		abort(400)
-	uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-	return redirect(url_for('index'))
+@app.route('/upload-image', methods =['GET', 'POST'])
+def upload_image():
+	if request.method == "POST":
+		if request.files:
+			print (request.cookies.get("filesize"))
+			if not allowed_image_filesize(request.cookies.get("filesize")):
+				print("File exceeded maximum size")
+				abort(400)
+			image = request.files["image"]
+			#print(image)    #terminal shows <FileStorage: 'image.jpg' ('image/svg+xml')>
+			filename = secure_filename(image.filename)
+			#print(image.filename)		#terminal shows the filename
+			if filename != "":
+				file_ext = os.path.splitext(filename)[1]
+				if file_ext not in app.config['ALLOWED_IMAGE_EXTENSIONS']:
+					abort(400)
+				image.save(os.path.join(app.config['IMAGE_UPLOADS'], filename))
+				print("image saved")    #just a msg on terminal to keep track
+				return redirect(request.url)
+			else:
+				print("image must have a filename")
+				return redirect(request.url)
+	return render_template('upload_image.html')
+#23:09
+def allowed_image_filesize(filesize):
+	if int(filesize) <= app.config['MAX_IMAGE_FILESIZE']:
+		print("true")
+		return True
+	else:
+		print("false")
+		return False
 
 
+# Protected route
+@app.route('/protected')
+def protected():
+    token = request.headers.get('Authorization', None)
+    if not token:
+        return jsonify({'message': 'Missing authorization header'}), 401
+
+    # Decode token
+    try:
+        token = token.split(' ')[1]
+        data = jwt.decode(token, app.config['SECRET_KEY'])
+    except:
+        return jsonify({'message': 'Invalid token'}), 401
+
+    return jsonify({'message': 'Protected data'})
 
 
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename)) 
+      return 'file uploaded successfully'
 if __name__ == "__main__":
+
 	app.run(host ="localhost", port = int("5000"))
