186b425dbe1d4888601e12b8b7e832708231bea5
Shaleen Mathur <shaleenmathur26@csu.fullerton.edu>
2023-04-10 22:00:58 -0700

    added comments and fixed public page

--- app.py
+++ app.py
@@ -22,8 +22,9 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 app.secret_key = 'happykey'
 os.makedirs(os.path.join(app.instance_path, 'uploads'), exist_ok=True)
-#app.config['SECRET_KEY'] = 'tokenkey'
-
+app.config["IMAGE_UPLOADS"] = '../uploads' 
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF"] 
+app.config["MAX_IMAGE_FILESIZE"] = 5 * 1024 * 1024 
 
 # app.config['MYSQL_HOST'] = '127.0.0.1'
 # app.config['MYSQL_USER'] = 'root'
@@ -31,6 +32,7 @@ os.makedirs(os.path.join(app.instance_path, 'uploads'), exist_ok=True)
 # app.config['MYSQL_DB'] = 'test'
     # To connect MySQL database
 
+#from myproject
 conn = pymysql.connect(
         host='localhost',
         user='root', 
@@ -40,102 +42,25 @@ conn = pymysql.connect(
 
 cur = conn.cursor(cursor=pymysql.cursors.DictCursor)
 
-def token_required(f):
-	@wraps(f)
-	def decorated(*args, **kwargs):
-		token = session['token']
-		print('\n', token, '\n' )
-		if not token:
-			return abort(500)
 
-		try:
-			data = jwt.decode(token, app.secret_key , algorithms=["HS256"])
-		except:
-			return abort(404)
+#from myproject
+@app.errorhandler(401)
+def unauthorized(e):
+	return jsonify(error=str(e)), 401 	
 
-		return f(*args, **kwargs)
-	return decorated
+@app.errorhandler(404)
+def page_not_found(e):
+	return jsonify(error=str(e)), 404 	
+@app.errorhandler(500)
+def unexpected(e):
+	return jsonify(error=str(e)), 500 	
 
-app.config["IMAGE_UPLOADS"] = '../uploads' # configure image upload folder to uploads
-app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF"] # configure allowed image extensions
-app.config["MAX_IMAGE_FILESIZE"] = 5 * 1024 * 1024 # configure max image file size, 0.5MB
-
-def allowed_image(filename):
-	if not "." in filename:
-		return False
-	ext = filename.rsplit(".", 1)[1]
-	if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
-		return True
-	else:
-		return False
-
-def allowed_image_filesize(filesize):
-	if int(filesize) <=  app.config["MAX_IMAGE_FILESIZE"]:
-		return True
-	else: 
-		return False
-	
-@app.route('/')
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-	if request.method == 'POST':
-		if request.files:
-			#print(request.cookies)
-			print("\n",request.files,"\n")
-			print("\n",request.cookies.get("filesize"),"\n")
-			if not allowed_image_filesize(request.cookies.get("filesize")):
-				print("File exceeds maximum size")
-				return redirect(request.url)
-			image = request.files['image']
-			if image.filename == "":
-				print("Image must have a filename")
-				return redirect(request.url)
-			if not allowed_image(image.filename):
-				print("That image extension is not allowed")
-				return redirect(request.url)
-			else:
-				filename = secure_filename(image.filename)
-				print(f"\n Filename: {filename} \n")
-				print("\n BEFORE SAVE \n")
-				print(app.config["IMAGE_UPLOADS"])
-				#image.save(app.config["IMAGE_UPLOADS"])
-				
-				image.save(os.path.join(app.instance_path, 'uploads', filename))
-
-				print("\n AFTER SAVE \n")
-			print(image)
-			
-			return redirect(request.url)
-	return render_template('upload.html')
+@app.errorhandler(400)
+def invalid(e):
+	return jsonify(error=str(e)), 400 
 
 
-@app.route('/public')
-def public():
-
-	cur.execute('SELECT COUNT(id) FROM 449_db.accounts;')
-	conn.commit()
-	temp = cur.fetchone()
-	num = temp['COUNT(id)']
-	#print(f"\n{num}\n")
-
-	i = 1
-	all_accounts = []
-	for i in range(num+1):
-		
-		cur.execute('SELECT id, username FROM accounts WHERE id = % s', (str(i)))
-		conn.commit()
-		single_account = cur.fetchone()
-		#print(f"\n\n{i} \n acc: {single_account}\n\n")
-		all_accounts.append(single_account)
-	#print(f"\n {all_accounts} \n")
-	return render_template('public.html', all_accounts = all_accounts)
-
-@app.route('/protected')
-@token_required
-def protected():
-	return jsonify({'message': 'Only viewed by valid tokens!'})
-
+#from myproject
 @app.route('/login', methods =['GET', 'POST'])
 def login():
 	msg = ''
@@ -143,7 +68,6 @@ def login():
 		username = request.form['username']
 		password = request.form['password']
 		token = jwt.encode({'user': username, 'exp': datetime.datetime.utcnow()+ datetime.timedelta(minutes=30)}, app.secret_key , algorithm="HS256")
-		# cursor = cur.cursor(MySQLdb.cursors.DictCursor)
 		cur.execute('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password, ))
 		conn.commit()
 		account = cur.fetchone()
@@ -162,6 +86,7 @@ def login():
 			return render_template('index.html', msg = msg, token = session['token'])
 	return render_template('login.html', msg = msg)
 
+#from myproject
 @app.route('/logout')
 def logout():
     session.pop('loggedin', None)
@@ -169,6 +94,7 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
 
+#from myproject
 @app.route('/register', methods =['GET', 'POST'])
 def register():
 	msg = ''
@@ -182,7 +108,6 @@ def register():
 		state = request.form['state']
 		country = request.form['country']
 		postalcode = request.form['postalcode']
-		# cursor = cur.cursor(MySQLdb.cursors.DictCursor)
 		cur.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
 		conn.commit()
 		account = cur.fetchone()
@@ -194,30 +119,29 @@ def register():
 			msg = 'name must contain only characters and numbers !'
 		else:
 			cur.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s, % s, % s, % s, % s, % s, % s)', (username, password, email, organisation, address, city, state, country, postalcode, ))
-			# cur.commit()
 			conn.commit()
 			msg = 'You have successfully registered !'
 	elif request.method == 'POST':
 		msg = 'Please fill out the form !'
 	return render_template('register.html', msg = msg)
 
-
+#from myproject
 @app.route("/index")
 def index():
 	if 'loggedin' in session:
 		return render_template("index.html")
 	return redirect(url_for('login'))
 
-
+#from myproject
 @app.route("/display")
 def display():
 	if 'loggedin' in session:
-		# cursor = cur.cursor(MySQLdb.cursors.DictCursor)
 		cur.execute('SELECT * FROM accounts WHERE id = % s', (session['id'], ))
 		account = cur.fetchone()
 		return render_template("display.html", account = account,)
 	return redirect(url_for('login'))
 
+#from myproject
 @app.route("/update", methods =['GET', 'POST'])
 def update():
 	msg = ''
@@ -232,7 +156,6 @@ def update():
 			state = request.form['state']
 			country = request.form['country']
 			postalcode = request.form['postalcode']
-			# cursor = cur.cursor(MySQLdb.cursors.DictCursor)
 			cur.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
 			account = cur.fetchone()
 			if account:
@@ -243,36 +166,130 @@ def update():
 				msg = 'name must contain only characters and numbers !'
 			else:
 				cur.execute('UPDATE accounts SET username =% s, password =% s, email =% s, organisation =% s, address =% s, city =% s, state =% s, country =% s, postalcode =% s WHERE id =% s', (username, password, email, organisation, address, city, state, country, postalcode, (session['id'], ), ))
-				# cur.commit()
 				msg = 'You have successfully updated !'
 		elif request.method == 'POST':
 			msg = 'Please fill out the form !'
 		return render_template("update.html", msg = msg)
 	return redirect(url_for('login'))
 
-@app.route("/admin")
+#from myproject
+@app.route("/admin", methods =['GET', 'POST'])
 def admin():
-	if session['username'] == 'admin':
-		return render_template('admin.html')
+	if 'loggedin' in session and session['username'] == 'admin':
+		return render_template("admin.html")
+
 	else:
 		abort(401)
 
-@app.errorhandler(401)
-def custom_401(e):
-	return jsonify(error=str(e)), 401 	# not a admin
+# Defining the decorator function
+def token_required(f):
+	# Using the wraps decorator to maintain the function metadata
+	@wraps(f)
+	def decorated(*args, **kwargs):
+		# Getting the token from the session
+		token = session['token']
+		# Printing the token for debugging purposes
+		print('\n', token, '\n' )
+		# Checking if the token is present
+		if not token:
+			# Returning a 500 error if the token is not present
+			return abort(500)
 
-@app.errorhandler(404)
-def custom_404(e):
-	return jsonify(error=str(e)), 404 	# token is not found
+		try:
+			# Decoding the token using the app's secret key and HS256 algorithm
+			data = jwt.decode(token, app.secret_key , algorithms=["HS256"])
+		except:
+			# Returning a 404 error if the token cannot be decoded
+			return abort(404)
 
-@app.errorhandler(500)
-def custom_500(e):
-	return jsonify(error=str(e)), 500 	# server error
-										# invalid token
+		# Calling the original function with the arguments and keyword arguments
+		return f(*args, **kwargs)
+	# Returning the decorated function
+	return decorated
 
-@app.errorhandler(400)
-def custom_400(e):
-	return jsonify(error=str(e)), 400 # bad request
+
+# Check if the filename has an allowed image extension
+def allowed_image(filename):
+	# If there is no period (.) in the filename, it is not a valid image file
+	if not "." in filename:
+		return False
+	# Split the filename by the rightmost period to get the file extension
+	ext = filename.rsplit(".", 1)[1]
+	# Check if the file extension is in the list of allowed image extensions
+	if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+		return True
+	else:
+		return False
+
+# Check if the filesize is within the maximum allowed limit
+def allowed_image_filesize(filesize):
+	# Check if the filesize is less than or equal to the maximum allowed image filesize
+	if int(filesize) <=  app.config["MAX_IMAGE_FILESIZE"]:
+		return True
+	else: 
+		return False
+	
+@app.route('/')
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+	if request.method == 'POST':
+		# If a POST request is received
+		if request.files:
+			# Check if files were uploaded in the request
+			print("\n",request.files,"\n")
+			print("\n",request.cookies.get("filesize"),"\n")
+			# Check if the uploaded file is within the allowed file size
+			if not allowed_image_filesize(request.cookies.get("filesize")):
+				print("File exceeds maximum size")
+				return redirect(request.url)
+			# Get the uploaded image file from the request object
+			image = request.files['image']
+			# Check if the uploaded file has a filename
+			if image.filename == "":
+				print("Image must have a filename")
+				return redirect(request.url)
+			# Check if the uploaded file has an allowed image extension
+			if not allowed_image(image.filename):
+				print("That image extension is not allowed")
+				return redirect(request.url)
+			else:
+				# If the uploaded file is valid, save it to the server
+				filename = secure_filename(image.filename)
+				print(f"\n Filename: {filename} \n")
+				print("\n BEFORE SAVE \n")
+				print(app.config["IMAGE_UPLOADS"])
+				# Save the file to the server
+				image.save(os.path.join(app.instance_path, 'uploads', filename))
+				print("\n AFTER SAVE \n")
+			print(image)
+			# Redirect the user to the upload page
+			return redirect(request.url)
+	# If a GET request is received, render the upload.html template
+	return render_template('upload.html')
+
+
+@app.route('/public')
+def public():
+
+	cur.execute('SELECT COUNT(id) FROM 449_db.accounts;')
+	conn.commit()
+	temp = cur.fetchone()
+	num = temp['COUNT(id)']
+
+	i = 1
+	all_accounts = []
+	for i in range(num+1):
+		
+		cur.execute('SELECT id, username FROM accounts WHERE id = % s', (str(i)))
+		conn.commit()
+		single_account = cur.fetchone()
+		all_accounts.append(single_account)
+	return render_template('public.html', all_accounts = all_accounts)
+
+@app.route('/protected')
+@token_required
+def protected():
+	return jsonify({'message': 'Only viewed by valid tokens!'})
 
 if __name__ == "__main__":
 	app.run(host ="localhost", port = int("5000"))
